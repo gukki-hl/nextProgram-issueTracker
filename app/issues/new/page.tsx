eb41d2e867fffd01a065d39c36557010b23cc561
陈海连 <948722262@qq.com>
2025-09-01 19:57:17 +0800

    03-7 添加markdown编辑器

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,19 +1,29 @@
 "use client";
-import React from "react";
-import { TextField,TextArea, Button } from "@radix-ui/themes";
+
+import { TextField, Button } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 // @ts-ignore
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { useCallback, useState } from "react";
 
 const NewIssuePage = () => {
+  const [value, setValue] = useState("Initial value");
+  const onChange = useCallback((value: string) => {
+    // 定义 onChange 回调，接收新的文本
+    setValue(value); // 更新 value 状态
+  }, []); // 空依赖数组，保证函数引用不会在每次渲染时改变
   return (
-    <div className="max-w-xl space-y-3" >
+    // JSX 渲染部分
+    <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Search the docs…">
         <TextField.Slot>
           <MagnifyingGlassIcon height="16" width="16" />
         </TextField.Slot>
       </TextField.Root>
-      <TextArea size='3' placeholder="Reply to comment…" />
-      <Button>Submit</Button>
+      <SimpleMDE value={value} onChange={onChange} />{" "}
+      {/* Markdown 编辑器，绑定 value 和 onChange */}
+      <Button>Submit New Issue</Button>
     </div>
   );
 };
