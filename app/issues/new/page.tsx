980806b5d2344ec2005056f6f44e90208d724cde
陈海连 <948722262@qq.com>
2025-09-03 12:03:15 +0800

    03-10 实现客户端表单输入错误提示

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { TextField, Button, Callout } from "@radix-ui/themes";
+import { TextField, Button, Callout, Text } from "@radix-ui/themes";
 import dynamic from "next/dynamic";
 import "easymde/dist/easymde.min.css";
 // @ts-ignore
@@ -9,11 +9,12 @@ import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useCallback, useState } from "react";
 import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+// 定义表单类型
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 // 动态引入 SimpleMDE，关闭 SSR，避免 document 未定义错误
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
@@ -21,7 +22,14 @@ const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
 });
 
 const NewIssuePage = () => {
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [value, setValue] = useState("");
   const [error, setError] = useState("");
   const router = useRouter();
@@ -32,6 +40,7 @@ const NewIssuePage = () => {
 
   return (
     <div className="max-w-xl">
+      {/* 如果存在全局错误信息，显示红色提示框 */}
       {error && (
         <Callout.Root color="red" className="mb-5">
           <Callout.Text>{error}</Callout.Text>
@@ -46,6 +55,7 @@ const NewIssuePage = () => {
             //成功后跳转到/issues页面
             router.push("/issues");
           } catch (error) {
+            // 捕获异常，设置全局错误提示
             setError("An unexpected error occurred");
           }
         })}
@@ -56,9 +66,15 @@ const NewIssuePage = () => {
             <MagnifyingGlassIcon height="16" width="16" />
           </TextField.Slot>
         </TextField.Root>
+        {/* 标题字段错误提示 */}
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         {/* Markdown 编辑器（只在客户端渲染） */}
         <Controller
-          name="description"
+          name="description" //表单字段名
           control={control} //表单控制器
           render={({ field }) => (
             <SimpleMDE
@@ -71,7 +87,12 @@ const NewIssuePage = () => {
             />
           )}
         />
-
+        {/* Markdown 编辑器错误提示 */}
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
         {/* 提交按钮 */}
         <Button>Submit New Issue</Button>
       </form>
