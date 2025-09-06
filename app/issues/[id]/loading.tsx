877dc20480db41250e8a389c94adc27b3a3e265d
陈海连 <948722262@qq.com>
2025-09-06 15:40:06 +0800

    04-8 添加额外的skeleton

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import { Box, Card, Flex } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 const LoadingIssueDetailPage = () => {
   return (
-    <div>loading...</div>
-  )
-}
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Flex gap="5" my='2'>
+        <Skeleton width='5rem' />
+        <Skeleton width='8rem' />
+      </Flex>
+      <Card className="prose" mt="4">
+        <Skeleton count={5} />
+      </Card>
+    </Box>
+  );
+};
 
-export default LoadingIssueDetailPage
+export default LoadingIssueDetailPage;
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
+import { Box } from '@radix-ui/themes';
+import Skeleton from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css';
 const newIssueLodingPage = () => {
-  return (
-    <div>loading...</div>
-  )
+   return(
+    <Box className='max-w-xl'>
+      <Skeleton/>
+      <Skeleton height="20rem"/>
+    </Box>
+   )
 }
 
 export default newIssueLodingPage
