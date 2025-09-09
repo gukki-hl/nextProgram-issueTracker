3ee86927590d458c50aee25d0e1c72fcadbcd65e
陈海连 <948722262@qq.com>
2025-09-09 18:24:45 +0800

    06-2 新增删除确认框

--- app/issues/[id]/DeleteIssueButon.tsx
+++ app/issues/[id]/DeleteIssueButon.tsx
@@ -1,10 +1,36 @@
-import { Button } from '@radix-ui/themes'
-import React from 'react'
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import React from "react";
 
-const DeleteIssueButon = ({issueId}:{issueId:number}) => {
+const DeleteIssueButon = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color='red'>DeleteIssueButon</Button>
-  )
-}
+    <Button color="red">
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Issue</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content maxWidth="450px">
+          <AlertDialog.Title>Revoke access</AlertDialog.Title>
+          <AlertDialog.Description size="2">
+            Are you sure? This application will no longer be accessible and any
+            existing sessions will be expired.
+          </AlertDialog.Description>
 
-export default DeleteIssueButon
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant="solid" color="red">
+                Revoke access
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </Button>
+  );
+};
+
+export default DeleteIssueButon;
