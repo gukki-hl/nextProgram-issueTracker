b77f0ef175bb9366c807bf835298d850b0174bd2
陈海连 <948722262@qq.com>
2025-09-09 20:05:25 +0800

    06-4 处理删除错误提示

--- app/issues/[id]/DeleteIssueButon.tsx
+++ app/issues/[id]/DeleteIssueButon.tsx
@@ -1,48 +1,73 @@
 "use client";
+
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 
 const DeleteIssueButon = ({ issueId }: { issueId: number }) => {
-    const router = useRouter();
-  
-  return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger >
-        <Button color="red">Delete Issue</Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content maxWidth="450px">
-        <AlertDialog.Title>Revoke access</AlertDialog.Title>
-        <AlertDialog.Description size="2">
-          Are you sure? This application will no longer be accessible and any
-          existing sessions will be expired.
-        </AlertDialog.Description>
+  const [error, setError] = useState(false);
+  const router = useRouter();
 
-        <Flex gap="3" mt="4" justify="end">
-          <AlertDialog.Cancel>
-            <Button variant="soft" color="gray">
-              Cancel
-            </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action >
-            <Button
-              variant="solid"
-              color="red"
-              onClick={async () => {
-                if (issueId) {
-                  await axios.delete("/api/issues/" + issueId);
-                  //成功后跳转到/issues页面
-                  router.push("/issues");
-                }
-              }}
-            >
-              Revoke access
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+  const deleteIssue = async () => {
+    try {
+      // throw new Error();
+      if (issueId) {
+        await axios.delete("/api/issues/" + issueId);
+        //成功后跳转到/issues页面
+        router.push("/issues");
+        router.refresh(); //刷新缓存
+      }
+    } catch {
+      setError(true);
+    }
+  };
+
+  return (
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Issue</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content maxWidth="450px">
+          <AlertDialog.Title>Revoke access</AlertDialog.Title>
+          <AlertDialog.Description size="2">
+            Are you sure? This application will no longer be accessible and any
+            existing sessions will be expired.
+          </AlertDialog.Description>
+
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant="solid" color="red" onClick={deleteIssue}>
+                Revoke access
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content maxWidth="450px" align="center">
+          <AlertDialog.Title>Delete fail</AlertDialog.Title>
+          <AlertDialog.Description size="1">
+            This issue cannot delete
+          </AlertDialog.Description>
+          <Button
+            variant="soft"
+            color="gray"
+            mt="4"
+            onClick={() => setError(false)}
+          >
+            OK
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
