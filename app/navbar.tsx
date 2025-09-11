fc3d3227948bebc94828b57bc37a7ed1dea3c1b7
陈海连 <948722262@qq.com>
2025-09-11 15:07:02 +0800

    07-6 重构导航栏

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -7,13 +7,6 @@ import React from "react";
 import { LuBug } from "react-icons/lu";
 import { Avatar, Box, DropdownMenu, Flex, Text } from "@radix-ui/themes";
 const navbar = () => {
-  const { status, data: session } = useSession();
-  const currentPath = usePathname(); //获取当前路径
-  const LINKS = [
-    { label: "Dashboard", href: "/" },
-    { label: "issues", href: "/issues/list" },
-  ];
-  if (status === "loading") return null;
   return (
     <nav className="border-b mb-5 px-5 h-14 py-3 ">
       <Flex justify="between">
@@ -21,51 +14,71 @@ const navbar = () => {
           <Link href="/">
             <LuBug size={20} />
           </Link>
-          <ul className="flex space-x-6">
-            {LINKS.map((link) => (
-              <li key={link.href}>
-                <Link
-                  href={link.href}
-                  className={classNames({
-                    "text-zinc-900": link.href === currentPath,
-                    "text-zinc-500": link.href !== currentPath,
-                    "hover:text-zinc-800 transition-colors": true,
-                  })}
-                >
-                  {link.label}
-                </Link>
-              </li>
-            ))}
-          </ul>
+          <NavLink />
         </Flex>
-        <Box>
-          {status === "authenticated" && (
-            <DropdownMenu.Root>
-              <DropdownMenu.Trigger>
-                <Avatar
-                  src={session.user?.image!}
-                  fallback="?"
-                  size="3"
-                  radius="full"
-                  className="cursor-pointer"
-                />
-              </DropdownMenu.Trigger>
-              <DropdownMenu.Content>
-                <DropdownMenu.Label>
-                  <Text size="2">{session.user?.email}</Text>
-                </DropdownMenu.Label>
-                <DropdownMenu.Item>
-                  <Link href="/api/auth/signout">SignOut</Link>
-                </DropdownMenu.Item>
-              </DropdownMenu.Content>
-            </DropdownMenu.Root>
-          )}
-          {status === "unauthenticated" && (
-            <Link href="/api/auth/signin">Login</Link>
-          )}
-        </Box>
+        <AuthStatus />
       </Flex>
     </nav>
+  );
+};
+const NavLink = () => {
+  const currentPath = usePathname(); //获取当前路径
+  const LINKS = [
+    { label: "Dashboard", href: "/" },
+    { label: "issues", href: "/issues/list" },
+  ];
+  return (
+    <ul className="flex space-x-6">
+      {LINKS.map((link) => (
+        <li key={link.href}>
+          <Link
+            href={link.href}
+            className={classNames({
+              "nav-link": true,
+              "!text-zinc-900": link.href === currentPath,
+            })}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+  if (status === "loading") return null;
+  if (status === "unauthenticated")
+    return (
+      <Link className="nav-link" href="/api/auth/signin">
+        Login
+      </Link>
+    );
+
+  return (
+    <Box>
+      {status === "authenticated" && (
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Avatar
+              src={session.user?.image!}
+              fallback="?"
+              size="3"
+              radius="full"
+              className="cursor-pointer"
+            />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Label>
+              <Text size="2">{session.user?.email}</Text>
+            </DropdownMenu.Label>
+            <DropdownMenu.Item>
+              <Link href="/api/auth/signout">SignOut</Link>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      )}
+    </Box>
   );
 };
 
