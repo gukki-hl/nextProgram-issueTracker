6065149bf20c2b86fa22ccae046fed356aea3520
陈海连 <948722262@qq.com>
2025-09-14 21:33:06 +0800

    09-3 根据点击的column参数排序

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,11 +2,18 @@ import { prisma } from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import { IssueStatusBadge, Link } from "@/app/component";
 import IssuesAction from "./IssuesAction";
-import { Status } from "@prisma/client";
-
+import { Issue, Status } from "@prisma/client";
+import NextLink from "next/link";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
 interface IssuesPageProps {
-  searchParams: { status: Status };
+  searchParams: { status: Status; orderBy?: keyof Issue };
 }
+
+const columns: { label: string; value: keyof Issue; className?: string }[] = [
+  { label: "Issue", value: "title" },
+  { label: "Status", value: "status", className: "hidden md:table-cell" },
+  { label: "Create", value: "createdAt", className: "hidden md:table-cell" },
+];
 
 export default async function IssuesPage({ searchParams }: IssuesPageProps) {
   const params = await searchParams;
@@ -25,13 +32,18 @@ export default async function IssuesPage({ searchParams }: IssuesPageProps) {
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Create
-            </Table.ColumnHeaderCell>
+            {columns.map((column) => (
+              <Table.ColumnHeaderCell key={column.value}>
+                <NextLink
+                  href={{ query: { ...params, orderBy: column.value } }}
+                >
+                  {column.label}
+                </NextLink>
+                {column.value === params.orderBy && (
+                  <ArrowUpIcon className="inline" />
+                )}
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
