723ba66f5b8d5fc2f7d75f7fea3bb35eaffc5d09
陈海连 <948722262@qq.com>
2025-09-16 19:02:51 +0800

    11-2 使用react缓存优化性能

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,6 +7,7 @@ import DeleteIssueButon from "./DeleteIssueButon";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOption";
 import AssignSelect from "./AssignSelect";
+import { cache } from "react";
 
 //params 是 Next.js 动态路由传递进来的参数
 // 比如路径 /issues/123 => params.id = "123"
@@ -14,14 +15,19 @@ interface Props {
   params: { id: string };
 }
 
+const fetchUser = cache((issueId: number) =>
+  prisma.issue.findUnique({
+    where: { id: issueId },
+  })
+);
+
 const IssueDetailPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions);
   const { id } = await params;
   //使用prisma查询数据库issue表里的id
   //路径输入的 params.id 是string，用parseInt 转为number
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(id) }, // 按主键 id 唯一查询
-  });
+
+  const issue = await fetchUser(parseInt(id)); // 按主键 id 唯一查询
   //如果没找到输入的id路径，触发next.js内置的404页面
   if (!issue) notFound();
 
@@ -44,9 +50,7 @@ const IssueDetailPage = async ({ params }: Props) => {
 };
 
 export async function generateMetadata({ params }: Props) {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchUser(parseInt(params.id));
 
   return {
     title: issue?.title,
