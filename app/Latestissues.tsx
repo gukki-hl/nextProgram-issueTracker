3e8f45603e67279a7bbd6f7414b132a2beec09b9
陈海连 <948722262@qq.com>
2025-09-16 15:27:52 +0800

    10-1 创建最新问题组件

---
+++ app/Latestissues.tsx
@@ -0,0 +1,46 @@
+import { prisma } from "@/prisma/client";
+import { Table, Flex, Avatar,Card,Heading } from "@radix-ui/themes";
+import Link from "next/link";
+import { IssueStatusBadge } from "./component";
+
+const Latestissues = async () => {
+  const issues = await prisma.issue.findMany({
+    take: 5,
+    orderBy: { createdAt: "desc" },
+    include: { assignedToUser: true },
+  });
+
+  return (
+    <Card>
+      <Heading size="6" mb="4">
+        Latest Issues
+      </Heading>
+      <Table.Root>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                <Flex justify="between">
+                  <Flex direction="column" align="start" gap="2">
+                    <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                    <IssueStatusBadge status={issue.status} />
+                  </Flex>
+                  {issue.assignedToUser && (
+                    <Avatar
+                      src={issue.assignedToUser.image!}
+                      fallback="?"
+                      size={"2"}
+                      radius="full"
+                    />
+                  )}
+                </Flex>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </Card>
+  );
+};
+
+export default Latestissues;
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-import Pagination from "./component/Pagination";
+import Latestissues from "./Latestissues";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
+export default function Home() {
   return (
-    <Pagination
-      itemCount={100}
-      pageSize={10}
-      currentPage={parseInt(searchParams.page)}
-    />
+    <Latestissues/>
   );
 }
