f4843a35457d4fa1f0d1ae1ec742fe54eb200b86
陈海连 <948722262@qq.com>
2025-09-18 09:36:45 +0800

    11-6 构建前bug fix

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -9,5 +9,5 @@ import authOptions from "@/app/auth/authOption";
 import NextAuth from "next-auth";
 
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
