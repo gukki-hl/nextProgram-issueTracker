1fd67e51f1daeb5a308186c4796322f423d590e3
陈海连 <948722262@qq.com>
2025-09-25 20:35:38 +0800

    解决本地环境依赖到生产的问题，删除硬编码代理设置修复生产中的 ECONNREFUSED 127.0.0.1:7890

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,9 +1,4 @@
 
-// 必须放在第一行，在任何 import 之前
-process.env.GLOBAL_AGENT_HTTP_PROXY = "http://127.0.0.1:7890";
-process.env.GLOBAL_AGENT_HTTPS_PROXY = "http://127.0.0.1:7890";
-require("global-agent/bootstrap");
-
 
 import authOptions from "@/app/auth/authOption";
 import NextAuth from "next-auth";
